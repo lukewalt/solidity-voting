7af2d24175851981698b28264f41b1914fefaa02
Luke Woodward <luke.wdwrd@gmail.com>
2017-07-10 21:26:31 -0500

    react app init, web3 install

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Web3 from 'web3';
+
+
+const ETHEREUM_PROVIDER = [];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">VOTE</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
