bbff0b9828a3a971ca4bbb6adca3faa91dc4516d
Luke Woodward <luke.wdwrd@gmail.com>
2017-07-13 10:23:54 -0500

    update UI from event emitted by contract vote function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import _ from 'lodash'
 
 
 const ETHEREUM_PROVIDER = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const BALLOT_ADDRESS = '0xc5d6965a2aff480d0000d0160fcab89fe86403a6';
+const BALLOT_ADDRESS = '0x29d6f06e33fc4c85ff4c903dbf84e14c8426f338';
 const BALLOT_ABI = [{"constant":false,"inputs":[{"name":"prop","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"proposals","outputs":[{"name":"name","type":"bytes32"},{"name":"voteCount","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"chairperson","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"winningProposal","outputs":[{"name":"winningProp","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"weight","type":"uint256"},{"name":"voted","type":"bool"},{"name":"delegate","type":"address"},{"name":"vote","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"returnWinner","outputs":[{"name":"winnerName","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getTotalCurrentVotes","outputs":[{"name":"totalCurrentVotes","type":"uint256[]"},{"name":"ballotNameTotals","type":"bytes32[]"}],"payable":false,"type":"function"},{"inputs":[{"name":"proposalNames","type":"bytes32[]"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"voter","type":"address"},{"indexed":false,"name":"proposal","type":"uint256"},{"indexed":false,"name":"dateCasted","type":"uint256"}],"name":"voteCasted","type":"event"}]
 const BALLOT = ETHEREUM_PROVIDER.eth.contract(BALLOT_ABI).at(BALLOT_ADDRESS);
 const coinbase = ETHEREUM_PROVIDER.eth.coinbase;
@@ -21,36 +21,56 @@ class App extends Component {
     }
   }
 
+
+
   componentDidMount() {
 
-    let initBallotAggregates = BALLOT.getTotalCurrentVotes()[0]
-    let initBallotNames = BALLOT.getTotalCurrentVotes()[1];
+    console.log(BALLOT.getTotalCurrentVotes());
+
+    let initBallotAggregates = BALLOT.getTotalCurrentVotes()[0];
+    let convertedAggregates = [];
+    let ballotNames = BALLOT.getTotalCurrentVotes()[1];
+
+    initBallotAggregates.forEach((BigNumber, index) => {
+      convertedAggregates.push(parseInt(BigNumber.toString(10)))
+    })
 
     this.setState({
-      candidateNames: initBallotNames,
-      candidateAggregateVotes: initBallotAggregates
+      candidateNames: ballotNames,
+      candidateAggregateVotes: convertedAggregates
     })
+
 
     this._broadcastVote()
 
   }
 
+
   // calls event from contract
   _broadcastVote() {
+    // call watch on event from contract
     BALLOT.voteCasted({ fromBlock: ETHEREUM_PROVIDER.eth.currentBlock, toBlock: 'latest' }).watch((err, res) => {
-      console.log(res.args);
+
+      let voteIndex = res.args.proposal.toString(10);
+      let updatedAggregate = this.state.candidateAggregateVotes
+
+      updatedAggregate.splice(voteIndex, 1, this.state.candidateAggregateVotes[voteIndex] + 1);
+
+      this.setState({
+        candidateAggregateVotes: updatedAggregate
+      });
+
     })
   }
 
   _vote(proposalIndex) {
-    console.log('voted', proposalIndex + 1);
     this.setState({
       isButtonDisabled: true
     });
-    // BALLOT.vote(proposalIndex, {from: coinbase, gas: 2100000}, (err, res) => {
-    //   console.log(res);
-    //   console.log(err);
-    // })
+    BALLOT.vote(proposalIndex, {from: coinbase, gas: 2100000}, (err, res) => {
+      console.log(res);
+      console.log(err);
+    })
   }
 
 
@@ -60,12 +80,12 @@ class App extends Component {
     // creates empty array to push JSX from state
     let candidates = [];
     let candidateVotes = [];
-    // pushes values of candidate
+    // pushes 'value', value, 'index', indexs of candidate
     _.each(this.state.candidateNames, (value, index) => {
       candidates.push(
         <div key={index}>
           <h1>{ETHEREUM_PROVIDER.toAscii(value)}</h1>
-          <h2>{this.state.candidateAggregateVotes[index].toString(10)}</h2>
+          <h2>{this.state.candidateAggregateVotes[index]}</h2>
           <button onClick={()=>{this._vote(index)}} disabled={this.state.isButtonDisabled} >Vote</button>
         </div>
       )
